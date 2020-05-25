c2358cc2d57c60ac749087de3706e0cfbff581b2
Victor Freitas <victor.freitas@autocom-mg.com.br>
2020-05-25 19:08:42 -0300

    Cria mobile da aplicacao

--- backend/src/controllers/incident.ctrl.js
+++ backend/src/controllers/incident.ctrl.js
@@ -2,28 +2,34 @@ const connection = require('../database/connection');
 
 module.exports = {
   async list(request, response) {
-    const { page = 3 } = request.query;
+    const { page } = request.query;
 
     const [count_incidents] = await connection('incidents').count();
 
-    // console.log(count_incidents);
+    console.log(count_incidents);
 
-    const incidents = await connection('incidents')
-      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
-      .limit(5)
-      .offset((page - 1) * 5)
-      .select([
-        'incidents.*',
-        'ongs.name',
-        'ongs.email',
-        'ongs.whatsapp',
-        'ongs.city',
-        'ongs.uf'
-      ]);
+    try {
 
-    response.header('X-Total-Count', count_incidents['count(*)']);
+      const incidents = await connection('incidents')
+        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+        .limit(5)
+        .offset((page - 1) * 5)
+        .select([
+          'incidents.*',
+          'ongs.name',
+          'ongs.email',
+          'ongs.whatsapp',
+          'ongs.city',
+          'ongs.uf'
+        ]);
 
-    return response.json(incidents);
+      response.header('X-Total-Count', count_incidents['count(*)']);
+
+      return response.json(incidents);
+    } catch (err) {
+      console.log(err);
+    }
+
   },
 
   async create(request, response) {
